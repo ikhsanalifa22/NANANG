e5815abf8240bc9feaa1c985c09ac46391052ea9
ikhsanalifa33 <ikhsanalifa33@gmail.com>
2024-03-23 14:12:07 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,31 @@
+
+	<footer style="border-top: 4px solid #0C0A0A;  ">
+		<div class="container" style="padding-bottom: 50px;">
+			<div class="row">
+				<div class="col-md-4">
+					<h3 style="color: #color: #0C0A0A;"><b>BERITAANGKASA STORE</b></h3>
+					<p>Jl. Raya padjajaran no 217</p>
+					<p><i class="glyphicon glyphicon-earphone"></i> +628979860572</p>
+					<p><i class="glyphicon glyphicon-envelope"></i> beritaangkasa@gmail.com</p>
+				</div>
+				<div class="col-md-4">
+					<h5><b></b></h5>
+					<p><a href=""  style="color: #000"></a></p>
+					<p><a href=""  style="color: #000"></a></p>
+					<p><a href=""  style="color: #000"></a></p>
+				</div>
+
+				<div class="col-md-4">
+					
+				</div>
+			</div>
+
+		</div>
+
+		<div class="copy" style="background-color: #0C0A0A; padding: 5px; color: #fff; text-align: center;">
+			<span>&copy; BERITAANGKASA STORE</span>
+		</div>
+	</footer>
+
+</body>
+</html>
